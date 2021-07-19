8b2a0e35422eb201588a8e43404e583031608da3
Jeroen Bos <jeroen@deepinsight.io>
2021-07-19 13:54:09 +0200

    Fix 2 bugs

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,4 @@
-export DATASET=demo\
+export DATASET=demo
 export GPU_IDS="-1"
 
 
@@ -6,4 +6,4 @@ export GPU_IDS="-1"
 export EPOCH=latest   # can also be the literal "latest"
 export STYLE="14x14"
 export DISPLAY_ID="1"
-export DISPLAY_HOST="visdom"  # name of docker container
+export DISPLAY_HOST="http://visdom"  # name of docker container
